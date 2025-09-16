a4548d9984c2d8256cc9aad2e9530d155ffbc3ea
sonjakomlen <komlensonja@gmail.com>
2025-09-16 23:52:29 +0200

    additional migrations for columns, constraints & foreign keys

---
+++ database/migrations/2025_09_03_103558_add_foreign_ids_to_transactions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->foreignId('account_id')->constrained('accounts')->cascadeOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('counterparty_account_id')->nullable()->constrained('accounts')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            //
+        });
+    }
+};
