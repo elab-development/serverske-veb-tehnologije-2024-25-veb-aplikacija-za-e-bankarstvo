8a9d1dbae3cf0292ba001c046e1ad8d682b85dd4
sonjakomlen <komlensonja@gmail.com>
2025-09-16 23:51:20 +0200

    account model & migration

---
+++ database/migrations/2025_09_03_102641_create_accounts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->enum('currency', ['RSD', 'EUR', 'USD', 'CHF', 'JPY'])->index();
+            $table->bigInteger('balance_minor')->default(0);
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
